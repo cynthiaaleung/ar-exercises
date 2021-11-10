d4b6f6cb2f9d12fb8404155c39d2310d40d9f7ed
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-11-09 17:43:26 -0800

    complete exercise

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Cynthia", last_name: "Leung", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Jones", hourly_rate: 50)
+
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store2.employees.create(first_name: "James", last_name: "Doe", hourly_rate: 70)
+@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 40)
+@store2.employees.create(first_name: "Ann", last_name: "Smith", hourly_rate: 30)
